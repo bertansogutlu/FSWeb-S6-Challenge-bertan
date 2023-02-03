42a20db8a214aa58e33034beb1fa3e913c0785c2
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-03 16:22:47 +0300

    data cekildi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,6 +9,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [karakterler, setKarakterler] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+  .then(function (response) {
+    const data = response.data;
+    setKarakterler(data);
+    console.log(data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  }, []);
 
   return (
     <div className="App">
