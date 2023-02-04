c43301ac678456a1b341d7486386a56e43ca7f84
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-04 16:36:34 +0300

    css eklendi

--- src/karakter.js
+++ src/karakter.js
@@ -3,17 +3,24 @@ import styled from "styled-components";
 
 const ScKarakter = styled.div`
 div {
-    border: 2px solid black;
+    border: 0.2rem solid black;
     max-width: 50%;
     margin: 2rem auto;
     background: rgba(0,0,0,0.2);
     font-size: 2rem;
+    border-radius: 1rem;
+    cursor: pointer;
 }
 h2 {
     font-size: 1.5rem;
 }
+h2:hover {
+    transition: scale 1s;
+    scale: 1.2;
+}
 li {
     font-size: 1rem;
+    list-style-type: none;
 }
 `
 
